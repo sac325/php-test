0efd88463217b4ae27179a8e8db9711dd63fa240
unknown <you@example.com>
2020-03-21 19:45:05 -0300

    create migrations

---
+++ database/migrations/2020_03_21_205106_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('rut');
+            $table->string('name');
+            $table->string('lastName');
+            $table->integer('age');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('no action');
+           // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
